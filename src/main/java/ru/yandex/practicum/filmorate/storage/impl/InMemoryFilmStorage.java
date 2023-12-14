05015f3125e0f46cbc2f012e3f39c51709014e54
Alexandro <naumich1984@yandex.ru>
2023-12-14 21:55:50 +0700

    fix: fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/InMemoryFilmStorage.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum.filmorate.storage.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NoFilmFoundException;
-import ru.yandex.practicum.filmorate.exception.NoUserFoundException;
 import ru.yandex.practicum.filmorate.exception.NoUserLikesFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.time.LocalDate;
